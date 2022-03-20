3ecc6ccad4461facafe479b99d881bc92e99ced8
Nguyen Hoang Long <lonnguyen.030997@gmail.com>
2022-03-20 11:43:57 +0700

    verson_2

--- js/index.js
+++ js/index.js
@@ -1,35 +1,47 @@
-document.querySelector('#signup-form .actions ul li:last-child').onclick = () => {
-    let arrInput = document.querySelectorAll('#signup-form input, #signup-form select');
-    var arrTitle = ['Tên', 'Họ', 'Email', 'Số điện thoại', 'Ngày sinh', 'Tháng sinh', 'Năm sinh', 'Địa chị', 'Thông tin di chuyển', 'Nghi nhiễm']
-    let infomation = {};
-    for (let input of arrInput){
-        let{id, value} = input;
-        infomation = {...infomation, [id]: value}
-    }
-    console.log(infomation)
+document.querySelector("#signup-form .actions ul li:last-child").onclick = () => {
+    let arrInput = document.querySelectorAll("#signup-form input, #signup-form select");
 
-    let contentHTML =
-    `<table class="mt-3">
-        <tr class="decorate">
-            <th width="80px">STT</th>
-            <th width="250px">Nội dung khai báo</th>
-            <th width="400px">Thông tin người khai báo</th>
-            <br>
-        </tr>
-    </table>`;
-    let count = 1;
-    for(let key in infomation){
-        contentHTML +=
-        `<table>
+    let infomation = {};
+    for (let input of arrInput) {
+      let { id, value } = input;
+      infomation = { ...infomation, [id]: value };
+    }
+
+    let contentHTML = `
+        <table class="mt-3">
             <tr class="decorate">
-                <td width="80px">${count++}</td>
-                <td width="250px">${key}</td>
-                <td width="400px">${infomation[key]}</td>
+                <th width="80px">STT</th>
+                <th width="275px">Nội dung khai báo</th>
+                <th width="450px">Thông tin người khai báo</th>
                 <br>
             </tr>
-        </table>`
+        </table>`;
+
+    let arrTitle = {
+      ten: "Tên",
+      ho: "Họ",
+      email: "Email",
+      soDienThoai: "Số điện thoại",
+      ngay: "Ngày sinh",
+      thang: "Tháng sinh",
+      nam: "Năm sinh",
+      diaChi: "Địa chỉ",
+      thongTinDiChuyen: "Thông tin chi chuyển",
+      loaiNhiem: "Nghi nhiễm",
+    };
+
+    let count = 1;
+    for (let key in infomation) {
+      contentHTML += `
+        <table>
+            <tr class="decorate">
+                <td width="80px">${count++}</td>
+                <td width="275px">${arrTitle[key]}</td>
+                <td width="450px">${infomation[key]}</td>
+                <br>
+            </tr>
+        </table>`;
     }
     
-    console.log(contentHTML)
-    document.querySelector('.donate-us').innerHTML = contentHTML;
-}
+    document.querySelector(".donate-us").innerHTML = contentHTML;
+  };
